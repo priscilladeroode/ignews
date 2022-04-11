703a3669312228bd89ae639bd191896c0c87e55c
Priscilla de Roode <priscilladeroode@gmail.com>
2022-04-10 22:27:21 -0300

    save user data to db

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
 import NextAuth from 'next-auth'
+import { query as q } from 'faunadb'
 import GithubProvider from 'next-auth/providers/github'
+
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
   providers: [
@@ -12,5 +15,27 @@ export default NextAuth({
         }
       }
     })
-  ]
+  ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index('user_by_email'), q.Casefold(user.email))
+              )
+            ),
+            q.Create(q.Collection('users'), { data: { email } }),
+            q.Get(q.Match(q.Index('user_by_email'), q.Casefold(user.email)))
+          )
+        )
+        return true
+      } catch (_) {
+        return false
+      }
+    }
+  }
 })
